fcd2d799d9570569bb71f32267cd8406fda9f9bc
christian-2001 <villegaschristian16@gmail.com>
2026-05-26 03:45:03 -0300

    update: actualizado barra de busqueda
    permite buscar publicaciones que coincida con lo que ingresó el usuario sin importar la forma en como está escrito(no funciona con etiquetas)

--- controllers/busqueda/busquedaController.js
+++ controllers/busqueda/busquedaController.js
@@ -7,42 +7,40 @@ import { Op } from "sequelize"
 
 export async function buscarPost(req, res) {
 
-    let postQuery = (req.query.buscador) ? req.query.buscador : ""
+    let postQuery = (req.query.buscador) ? req.query.buscador.trim() : ""
     let postTag = (req.query.etiqueta) ? req.query.etiqueta : ""
     let queryResult = ""
 
     //Comprobamos si lo que vino desde el frontend es via barra de busqueda o el valor obtenido al clickear una etiqueta
 
-    //Si el usuario no ha ingresado nada, la pagina lo redirige al "home"
-    if (postQuery == "") {
+    //Si el usuario no ha ingresado nada en la barra de busqueda, la pagina lo redirige al "home"
+    if (postTag){
+        //Si la etiqueta se obtuvo al hacerle click recibiendo su nombre
+        //Filtrar las publicaciones que contenga la etiqueta recibida
+        getPostTag(postTag)
+            .then((data) => {
+                queryResult = data
+                res.status(200).render("index", { queryResult: queryResult, query: `#${postTag}` })
+            })
+            .catch((error) => res.status(200).send("Ocurrio un error al mostrar las publicaciones"))
+    } else if (postQuery == "") {
 
         res.redirect("/")
-    
+
         //Si el valor recibido, desde la barra de busqueda empieza con almohadilla o hash(#), 
         // o proviene del click a una etiqueta recibiendo su nombre
-    } else if ((postQuery && postQuery.startsWith("#")) || postTag) {
+    } else if ((postQuery && postQuery.startsWith("#"))) {
 
         //Si la etiqueta proviene de la barra de busqueda
-        if (!postTag) {
-            postQuery = postQuery.split("#")[1]
-            //Filtrar las publicaciones que coincidan con el valor obtenido en la barra de busqueda
-            getPostTag(postQuery)
-                .then((data) => {
-                    console.log(data)
-                    queryResult = data
-                    res.status(200).render("index", { queryResult: queryResult, query: `#${postQuery}` })
-                })
-                .catch((error) => res.status(200).send("Ocurrio un error al mostrar las publicaciones"))
-        } else {
-            //Si la etiqueta se obtuvo al hacerle click recibiendo su nombre
-            //Filtrar las publicaciones que contenga la etiqueta recibida
-            getPostTag(postTag)
-                .then((data) => {
-                    queryResult = data
-                    res.status(200).render("index", { queryResult: queryResult, query: `#${postTag}` })
-                })
-                .catch((error) => res.status(200).send("Ocurrio un error al mostrar las publicaciones"))
-        }
+        postQuery = postQuery.split("#")[1]
+        //Filtrar las publicaciones que coincidan con el valor obtenido en la barra de busqueda
+        getPostTag(postQuery)
+            .then((data) => {
+                console.log(data)
+                queryResult = data
+                res.status(200).render("index", { queryResult: queryResult, query: `#${postQuery}` })
+            })
+            .catch((error) => res.status(200).send("Ocurrio un error al mostrar las publicaciones"))
 
         //Si el valor recibido proviene de la barra de busqueda y no empieza con almohadilla o hash(#)
     } else if (postQuery) {
@@ -54,7 +52,7 @@ export async function buscarPost(req, res) {
                 res.status(200).render("index", { queryResult: queryResult, query: postQuery })
             })
 
-    }
+    } 
 }
 
 //Filtrar las publicaciones que contenga la etiqueta recibida
@@ -82,19 +80,19 @@ async function getPostQuery(query) {
             [Op.or]: [
                 {
                     '$Publicacion.titulo$': {
-                        [Op.like]: `%${query}%`
+                        [Op.iLike]: `%${query}%`
                     }
                 },
 
                 {
                     '$Publicacion.descripcion$': {
-                        [Op.like]: `%${query}%`
+                        [Op.iLike]: `%${query}%`
                     }
                 },
 
                 {
                     '$Usuario.nombre_usuario$': {
-                        [Op.like]: `%${query}%`
+                        [Op.iLike]: `%${query}%`
                     }
                 },
                 {
