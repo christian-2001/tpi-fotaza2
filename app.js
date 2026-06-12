1333501c47e6d79fba4821fffd62bc64e1314dc1
christian-2001 <villegaschristian16@gmail.com>
2026-06-12 03:59:43 -0300

    fix: cambio en el atributo "secure" de true a false

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         maxAge: 24 * 60 * 60 * 1000, // 24 horas
         httpOnly: true,
         sameSite: "lax",
